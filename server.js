cb866897c28cd7553d0137968befdbda18c08b63
jkulmus <kul18001@byui.edu>
2026-06-08 13:11:40 -0700

    Initial Express and EJS app setup

---
+++ server.js
@@ -0,0 +1,28 @@
+import "dotenv/config";
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const PORT = process.env.PORT || 3000;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "src/views"));
+
+app.get("/", (req, res) => {
+    res.render("index", {
+        title: "Emberhold Quest Portal"
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+});
