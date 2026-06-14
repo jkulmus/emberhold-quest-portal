2adf94bfe6472daa5fec6db7c36fffc621637900
jkulmus <kul18001@byui.edu>
2026-06-13 19:27:28 -0700

    Update quest model

--- src/models/quests/quests.js
+++ src/models/quests/quests.js
@@ -25,7 +25,36 @@ const getQuestById = async (id) => {
     return result.rows[0] || null;
 }
 
+const createQuest = async ({
+    title,
+    description,
+    difficulty,
+    reward,
+    duration_minutes,
+    max_party_size
+}) => {
+    const sql = `
+        INSERT INTO quests
+        (title, description, difficulty, reward, duration_minutes, max_party_size)
+        VALUES
+        ($1, $2, $3, $4, $5, $6)
+        RETURNING *;
+    `;
+
+    const result = await db.query(sql, [
+        title,
+        description,
+        difficulty,
+        reward,
+        duration_minutes,
+        max_party_size
+    ]);
+
+    return result.rows[0];
+};
+
 export {
     getAllQuests,
-    getQuestById
+    getQuestById,
+    createQuest
 };
