8adb06d085e4abce27d9af6f87c8bb5bf22df25f
jkulmus <kul18001@byui.edu>
2026-07-08 16:56:30 -0700

    updated and added admin features

---
+++ src/controllers/admin/users.js
@@ -0,0 +1,37 @@
+import {
+    getAllUsersWithRoles,
+    updateUserRole
+} from "../../models/userModel.js";
+
+const manageUsersPage = async (req, res) => {
+    try {
+        const users = await getAllUsersWithRoles();
+
+        res.render("admin/users", {
+            title: "Manage Guild Members",
+            users
+        });
+    } catch (error) {
+        console.error("Error loading users:", error);
+        res.status(500).send("Unable to load guild members")
+    }
+};
+
+const processRoleUpdate = async (req, res) => {
+    try {
+        await updateUserRole(req.params.id, req.body.role);
+
+        req.flash("success", "Guild member role undated");
+        res.redirect("/admin/users");
+    } catch (error) {
+        console.error("Error updating user role:", error);
+
+        req.flash("error", "Unable to update guild member role");
+        res.redirect("/admin/users");
+    }
+};
+
+export {
+    manageUsersPage,
+    processRoleUpdate
+};
