53d2561d19bce12d14c7b3563f0fa94fd2fc9dc3
jkulmus <kul18001@byui.edu>
2026-07-08 16:47:28 -0700

    updated user info

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -56,8 +56,46 @@ const getUserByEmailWithRole = async (email) => {
     return result.rows[0] || null;
 }
 
+const getAllUsersWithRoles = async () => {
+    const sql = `
+        SELECT
+            u.id,
+            u.name,
+            u.email,
+            u.created_at,
+            r.role_name
+        FROM users u
+        LEFT JOIN roles r
+            ON u.role_id = r.id
+        ORDER BY u.created_at DESC;
+    `;
+
+    const result = await db.query(sql);
+    return result.rows;
+};
+
+const updateUserRole = async (userId, roleName) => {
+    const sql = `
+        UPDATE users
+        SET
+            role_id = (
+                SELECT id
+                FROM roles
+                WHERE role_name = $1
+            ),
+            updated_at = CURRENT_TIMESTAMP
+        WHERE id = $2
+        RETURNING *;
+    `;
+
+    const result = await db.query(sql, [roleName, userId]);
+    return result.rows[0] || null;
+};
+
 export {
     createUser,
     getUserByEmail,
-    getUserByEmailWithRole
+    getUserByEmailWithRole,
+    getAllUsersWithRoles,
+    updateUserRole
 };
